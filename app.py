cabedc8a3507d54383484b859d4580d524240dfc
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-05-17 11:58:12 +0530

    POST ADDED

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 app = Flask(__name__)
 
+data = []
 @app.route('/')
 def index():
-  return "Hello World!"
+  return jsonify(data)
+
+@app.route('/data/add',methods = ['POST'])
+def add_data():
+  foo = dict()
+  if not data:
+    foo['id'] = 1
+  else:   
+    foo['id'] = data[-1]['id']+1
+  foo['ip'] = request.remote_addr
+  foo['data'] = request.json.get('data',)
+  data.append(foo)
+  return jsonify({'data': foo}), 201
 
 if __name__ == '__main__':
   app.run(debug=True)
